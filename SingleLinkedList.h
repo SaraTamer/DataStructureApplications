9649ccaf30bd6bf07369d7aa14bfd1e4706eec05
farahmohmed <102584837+farah2543@users.noreply.github.com>
2023-04-09 17:21:35 +0200

    single linked list

---
+++ SingleLinkedList.h
@@ -0,0 +1,344 @@
+//
+// Created by DELL on 4/9/2023.
+//
+
+#ifndef DATASTRUCTUREAPPLICATIONS_SINGLELINKEDLIST_H
+#define DATASTRUCTUREAPPLICATIONS_SINGLELINKEDLIST_H
+#include <iostream>
+using namespace std;
+
+
+#include <iostream>
+
+#define ll long long
+
+using namespace std;
+
+
+
+template <class T>
+class SingleLinkedList {
+private:
+    struct Node {
+        Node *next;
+        T value;
+
+        Node() {
+            value = 0;
+
+        }
+    };
+
+    Node *head = new Node();
+    Node *tail = new Node();
+    ll sz;
+
+public:
+    SingleLinkedList()
+    {
+        head = tail = nullptr;
+        sz =0;
+    }
+    void insertAtHead(T item);
+    void insertAtTail(T item);
+    void insertAt(T item , ll index);
+
+    void removeAtHead();
+    void removeAtTail();
+    void removeAt(ll index);
+
+    T retrieveAt(ll index);
+    void replaceAt(T item , ll index);
+
+    bool isExist(T item);
+    bool isItemAtEqual(T item , ll index);
+
+    void swap (int index1,int index2);
+
+    ll size(){return sz;}
+    bool isEmpty(){return (sz == 0);}
+
+    void print();
+
+    void clear();
+
+    ~SingleLinkedList();
+
+
+};
+
+
+template<class T>
+void SingleLinkedList<T>::insertAtHead(T item) {
+    Node *tmp = new Node;
+    tmp->value = item;
+    if (isEmpty()) {
+        head = tail = tmp;
+        tmp->next = nullptr;
+    } else {
+        tmp->next = head;
+
+        head = tmp;
+    }
+    sz++;
+}
+
+template<class T>
+void SingleLinkedList<T>::insertAtTail(T item) {
+    Node *tmp = new Node;
+    tmp->value = item;
+    if(isEmpty())
+    {
+        head = tail = tmp;
+        tmp->next = nullptr;
+    }
+    else
+    {
+        tail->next = tmp;
+        tmp->next = nullptr;
+        tail = tmp;
+    }
+    sz++;
+
+}
+
+template<class T>
+void SingleLinkedList<T>::insertAt(T item, long long int index) {
+    if(index > sz || index < 0)
+    {
+        cout << "Position out of scope!\n";
+        return;
+    }
+    if(index == 0)
+    {
+        insertAtHead(item);
+    }
+    else if(index == sz)
+    {
+        insertAtTail(item);
+    }
+    else {
+        Node *temp = head;
+        Node *newNode = new Node;
+        newNode->value = item;
+        for (ll i = 0; i < index - 1; i++)
+        {
+            temp = temp->next;
+        }
+        newNode->next = temp->next;
+        temp->next = newNode;
+
+        sz++;
+    }
+
+}
+
+template<class T>
+void SingleLinkedList<T>::removeAtHead() {
+    if (isEmpty()) {
+        cout << "list is empty nothing to be removed \n";
+        return;
+    }
+
+    else if (sz == 1){
+        delete head;
+        head = nullptr;
+        tail = head;
+        sz--;
+        return;
+    }
+    else{
+        Node *temp = head;
+        head = head->next;
+        free(temp);
+        sz--;
+    }
+
+
+}
+
+template<class T>
+void SingleLinkedList<T>::removeAtTail() {
+    if(isEmpty())
+    {
+        cout << "The list is empty nothing to be removed \n";
+        return;
+    }
+    else if(sz == 1)
+    {
+        delete head;
+        head = tail = nullptr;
+        sz--;
+        return;
+    }
+    else{
+        Node *temp = head;
+        while (temp->next != tail) {
+            temp = temp->next;
+        }
+        delete tail;
+        temp->next = nullptr;
+        tail = temp;
+        sz--;
+    }
+
+
+}
+
+template<class T>
+void SingleLinkedList<T>::removeAt(long long int index) {
+    if(index >= sz || index < 0)
+    {
+        cout << "Position out of scope!\n";
+        return;
+    }
+    else if(index == 0)
+    {
+        removeAtHead();
+        return;
+    }
+    else if(index == sz -1)
+    {
+        removeAtTail();
+        return;
+    }
+    else{
+        int counter = 0;
+        Node *temp = head;
+
+        while (counter < index - 1){
+            temp = temp->next;
+            counter++;
+        }
+
+        Node *target = temp->next;
+        temp->next = target->next;
+        delete target ;
+        sz--;
+    }
+
+
+}
+
+template<class T>
+T SingleLinkedList<T>::retrieveAt(long long int index) {
+    if(index >= sz || index < 0)
+    {
+        cout << "Position out of scope!\n";
+        exit(1);
+    }
+    if(isEmpty())
+    {
+        cout << "The list is EMPTY!\n";
+        exit(1);
+    }
+
+    Node* tmp = head;
+    for(ll i = 0; i < index; i++)
+    {
+        tmp = tmp->next;
+    }
+    return tmp->value;
+}
+
+template<class T>
+void SingleLinkedList<T>::replaceAt(T item, long long int index) {
+    if(index < 0 || index >= sz)
+    {
+        cout << "Position out of scope!\n";
+        return;
+    }
+
+    Node* tmp = head;
+    for(ll i = 0; i < index; i++)
+    {
+        tmp = tmp->next;
+    }
+    tmp->value = item;
+
+}
+
+template<class T>
+bool SingleLinkedList<T>::isExist(T item) {
+
+    Node* tmp = head;
+    for(ll i = 0; i < sz; i++)
+    {
+        if(tmp->value == item) {
+            return true;
+        }
+        tmp = tmp->next;
+    }
+    return false;
+}
+
+template<class T>
+bool SingleLinkedList<T>::isItemAtEqual(T item, long long int index) {
+    if(isEmpty())
+    {
+        cout << "The list is EMPTY!\n";
+        exit(1);
+    }
+    else if(index < 0 || index >= sz)
+    {
+        cout << "Position out of scope!\n";
+        exit(1);
+    }
+    else{
+        Node* tmp = head;
+        for(ll i = 0; i < index; i++)
+        {
+            tmp = tmp->next;
+        }
+        return (tmp->value == item);
+    }
+}
+
+
+template<class T>
+void SingleLinkedList<T>::swap(int index1, int index2) {
+
+
+}
+
+template<class T>
+void SingleLinkedList<T>::print() {
+    Node *temp = head;
+    while (temp != nullptr){
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+    cout << "Size of list: " << sz <<"\n";
+
+}
+
+
+template<class T>
+void SingleLinkedList<T>::clear() {
+    while (!isEmpty()){
+        removeAtTail();
+    }
+
+}
+
+
+
+
+template<class T>
+SingleLinkedList<T>::~SingleLinkedList() {
+    Node *tmp = new Node;
+    for(ll i = 0; i < sz; i++)
+    {
+        tmp = head;
+        head = head->next;
+        delete tmp;
+    }
+    tail = nullptr;
+    sz = 0;
+
+}
+
+
+
+#endif //DATASTRUCTUREAPPLICATIONS_SINGLELINKEDLIST_H
--- main.cpp
+++ main.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 #include "InfixToPostfix.h"
 #include "LongestValidParentheses.h"
+#include "SingleLinkedList.h"
 
 
 using namespace std;
 
 int main()
 {
-//// Stack Applications
+// Stack Applications
 
-    //// Infix to postfix testing
-
+    // Infix to postfix testing
+//
 //    string expression = "(3+2)+7/2*((7+3)*2)";
 //    string expression2 = "X^Y / (5*Z) + 2";
 //    string expression3 = "A + B * C + D";
 //    string expression4 = " (A + B) * (C + D)";
 //    cout << infixToPostfix(expression4);
 
-    //// longest valid parentheses testing
-
+    // longest valid parentheses testing
+//
 //    string expression = "(()";
 //    string expression2 = ")()())((()))";
 //    cout << longestValidParentheses(expression2);
